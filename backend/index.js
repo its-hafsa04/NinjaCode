143710c2d38477b0be8ad61cbb0a258f7d892c20
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2025-01-03 15:55:42 +0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,7 @@ const admin = require('firebase-admin');
 const cors = require('cors');
 
 // Initialize Firebase Admin SDK
-let serviceAccount;
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  serviceAccount = require('../leetcode-1bdec-firebase-adminsdk-orb1s-be580fc140.json');
-}
-
+const serviceAccount = require('../leetcode-1bdec-firebase-adminsdk-orb1s-be580fc140.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
