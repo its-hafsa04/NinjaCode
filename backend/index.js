e57fef43bee979bc69c284d8dc337ae0424dfad4
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2025-01-06 17:13:35 +0500

    added

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,19 @@ const cors = require('cors');
 require('dotenv').config();
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./leetcode-1bdec-firebase-adminsdk-orb1s-be580fc140.js');
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
