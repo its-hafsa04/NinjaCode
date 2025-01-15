0c73ef66de30bd852faefdd8719dc3bca1639428
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2025-01-15 22:12:08 +0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ admin.initializeApp({
 const app = express();
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://ninja-code-lake.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
