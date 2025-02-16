fb5eece44100a7dce3c6f5cc9863672795c7b5a6
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2025-02-16 18:18:11 +0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ admin.initializeApp({
 const app = express();
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
